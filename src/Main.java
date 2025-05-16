7167e2ef34dd96946b5f76afe88e357287d212db
Fujiwara takeru <241065@std.hi-joho.ac.jp>
2025-05-16 12:00:35 +0900

    Review4を作成

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
         Review1 rev1 = new Review1();
         Review2 rev2 = new Review2();
         Review3 rev3 = new Review3();
-        rev3.review(); //ここのレビューの値を変更する
+        Review4 rev4 = new Review4();
+
+
+        rev4.review(); //ここのレビューの値を変更する
     }
 }
---
+++ src/Review4.java
@@ -0,0 +1,15 @@
+public class Review4 {
+    public void review() {
+        int number1 = 5;
+        String str = "8888";
+        double PI =3.141592;
+        PI = 3.14;
+        float circ = 3.14F;
+        long a1 = 1000000000;
+        long a2 = 10;
+        boolean fig = true;
+        byte b = 35;
+        int num = 0x735;
+        char c = 'a';
+    }
+}
