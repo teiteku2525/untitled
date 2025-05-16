bb9e3a3346c2bedd1119b244a5c013303256dc82
Fujiwara takeru <241065@std.hi-joho.ac.jp>
2025-05-16 11:47:54 +0900

    Review3を作成

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Review1 rev1 = new Review1();
         Review2 rev2 = new Review2();
-        rev2.review(); //ここのレビューの値を変更する
+        Review3 rev3 = new Review3();
+        rev3.review(); //ここのレビューの値を変更する
     }
 }
---
+++ src/Review3.java
@@ -0,0 +1,7 @@
+public class Review3 {
+    public void review() {
+        System.out.println(10 - 7 * 2 + "7" + 5 * -10);
+        System.out.println(3 * 2 * 3 + 7 + "0" + 5 + 3);
+        System.out.println("7" + 3 * 2 + 5 + 6 / 2);
+    }
+}
